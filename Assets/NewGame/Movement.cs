d9f22f014472d09b20d230a6e7fbdd8b5bbb021c
Linyx-Multi <33364846+Linyx-Multi@users.noreply.github.com>
2023-06-14 14:41:26 +1200

    EAefa

--- Assets/NewGame/Movement.cs
+++ Assets/NewGame/Movement.cs
@@ -8,8 +8,8 @@ public class Movement : MonoBehaviour
     // Variables for different parts of the script, the names should explain enough, yeeeessss....?
 
     private float horizontal;
-    private float speed = 8f;
-    private float jumpingPower = 16f;
+    private float speed = 6f;
+    private float jumpingPower = 7f;
     private bool isFacingRight = true;
 
     // Cereal-ized Fields.. got a transformed, rigid mask..
@@ -22,15 +22,7 @@ public class Movement : MonoBehaviour
     {
         horizontal = Input.GetAxisRaw("Horizontal"); // Returns either 1, 0 or -1
 
-        Flip(); // DO A FLIP!
-    }
-
-    // FixedUpdate checks for speed and horizontal movement and translates to velocity. *MATH*
-    private void FixedUpdate()
-    {
-        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
-        
-        if(Input.GetButtonDown("Jump") && IsGrounded()) // no double-jumping, hopefully..
+        if (Input.GetButtonDown("Jump") && IsGrounded()) // no double-jumping, hopefully..
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpingPower); // jump in a direction, maybe?
         }
@@ -39,12 +31,23 @@ public class Movement : MonoBehaviour
         {
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f); // Gets X velo, and Y velo and multiplies by 0.5, jump high by holding.
         }
+
+        Flip(); // DO A FLIP!
     }
 
+    // FixedUpdate checks for speed and horizontal movement and translates to velocity. *MATH*
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+
+        /*s e c r e t  c o m m e n t*/
+    }
+    
     // are YOU grounded?
     private bool IsGrounded()
     {
-        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+        Debug.Log(Physics2D.OverlapCircle(groundCheck.position, 1f, groundLayer));
+        return Physics2D.OverlapCircle(groundCheck.position, 1f, groundLayer); // creates an overlap circle under the character to act as a ground check checksum.
     }
 
     // Do a barrel roll (Z or R twice) !
